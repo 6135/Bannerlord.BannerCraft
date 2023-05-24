b131d33d432218974b194a298e78fbbb3d276221
adwitkow <adamadik@poczta.fm>
2023-05-25 01:15:18 +0200

    Cleanup SmeltingVMPatch

--- Bannerlord.BannerCraft/Patches/SmeltingVMPatch.cs
+++ Bannerlord.BannerCraft/Patches/SmeltingVMPatch.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
-using System.Reflection;
 using Bannerlord.BannerCraft.ViewModels;
+using HarmonyLib;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Roster;
 using TaleWorlds.CampaignSystem.ViewModelCollection.WeaponCrafting.Smelting;
@@ -11,30 +11,61 @@ namespace Bannerlord.BannerCraft.Patches
 {
     internal class SmeltingVMPatch
     {
-        public static void RefreshListPostfix(SmeltingVM __instance)
+        static SmeltingVMPatch()
+        {
+            var isItemLockedMethod = AccessTools.Method(typeof(SmeltingVM), "IsItemLocked");
+            IsItemLocked = (vm, elem) => (bool)isItemLockedMethod.Invoke(vm, new object[] { elem });
+
+            var itemRosterField = AccessTools.Field(typeof(SmeltingVM), "_playerItemRoster");
+            GetPlayerItemRoster = vm => (ItemRoster)itemRosterField.GetValue(vm);
+
+            var onItemSelectionMethod = AccessTools.Method(typeof(SmeltingVM), "OnItemSelection");
+            GetOnItemSelectionAction = vm => AccessTools.MethodDelegate<Action<SmeltingItemVM>>(onItemSelectionMethod, vm);
+
+            var processLockItemMethod = AccessTools.Method(typeof(SmeltingVM), "ProcessLockItem");
+            GetProcessLockItemAction = vm => AccessTools.MethodDelegate<Action<SmeltingItemVM, bool>>(processLockItemMethod, vm);
+        }
+
+        private static Func<SmeltingVM, EquipmentElement, bool> IsItemLocked { get; }
+
+        private static Func<SmeltingVM, ItemRoster> GetPlayerItemRoster { get; }
+
+        private static Func<SmeltingVM, Action<SmeltingItemVM>> GetOnItemSelectionAction { get; }
+
+        private static Func<SmeltingVM, Action<SmeltingItemVM, bool>> GetProcessLockItemAction { get; }
+
+        public static void RefreshListPostfix(ref SmeltingVM __instance)
         {
             if (!Settings.Instance.AllowSmeltingOtherItems)
             {
                 return;
             }
 
-            BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic;
-            Func<EquipmentElement, bool> isItemLocked = (Func<EquipmentElement, bool>)typeof(SmeltingVM).GetMethod("IsItemLocked", bindingFlags).CreateDelegate(typeof(Func<EquipmentElement, bool>), __instance); ;
+            var smithingModel = Campaign.Current.Models.SmithingModel;
 
-            ItemRoster playerItemRoster = (ItemRoster)typeof(SmeltingVM).GetField("_playerItemRoster", bindingFlags).GetValue(__instance);
-
-            Action<SmeltingItemVM> onItemSelection = (Action<SmeltingItemVM>)typeof(SmeltingVM).GetMethod("OnItemSelection", bindingFlags).CreateDelegate(typeof(Action<SmeltingItemVM>), __instance);
-            Action<SmeltingItemVM, bool> processLockItem = (Action<SmeltingItemVM, bool>)typeof(SmeltingVM).GetMethod("ProcessLockItem", bindingFlags).CreateDelegate(typeof(Action<SmeltingItemVM, bool>), __instance);
+            var playerItemRoster = GetPlayerItemRoster(__instance);
+            var onItemSelection = GetOnItemSelectionAction(__instance);
+            var processLockItem = GetProcessLockItemAction(__instance);
 
             for (int i = 0; i < playerItemRoster.Count; i++)
             {
-                ItemRosterElement elementCopyAtIndex = playerItemRoster.GetElementCopyAtIndex(i);
-                if (!ArmorCraftingVM.ItemTypeIsWeapon(ArmorCraftingVM.GetItemType(elementCopyAtIndex.EquipmentElement.Item))
-                    && Campaign.Current.Models.SmithingModel.GetSmeltingOutputForItem(elementCopyAtIndex.EquipmentElement.Item).Any((x) => x > 0))
+                var elementCopyAtIndex = playerItemRoster.GetElementCopyAtIndex(i);
+                var item = elementCopyAtIndex.EquipmentElement.Item;
+                var itemType = ArmorCraftingVM.GetItemType(item);
+                var smeltingOutputs = smithingModel.GetSmeltingOutputForItem(item);
+                var givesOutput = smeltingOutputs.Any(output => output > 0);
+                if (!ArmorCraftingVM.ItemTypeIsWeapon(itemType) && givesOutput)
                 {
-                    bool isLocked = isItemLocked(elementCopyAtIndex.EquipmentElement);
-                    SmeltingItemVM item = new SmeltingItemVM(elementCopyAtIndex.EquipmentElement, onItemSelection, processLockItem, isLocked, elementCopyAtIndex.Amount);
-                    __instance.SmeltableItemList.Add(item);
+                    bool isLocked = IsItemLocked(__instance, elementCopyAtIndex.EquipmentElement);
+
+                    SmeltingItemVM smeltingItem = new SmeltingItemVM(
+                        elementCopyAtIndex.EquipmentElement,
+                        onItemSelection,
+                        processLockItem,
+                        isLocked,
+                        elementCopyAtIndex.Amount);
+
+                    __instance.SmeltableItemList.Add(smeltingItem);
                 }
             }
 
